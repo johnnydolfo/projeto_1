855d516493511b6f9d1596256f66b8242e999378
PICHAU <jrbergerandrade@gmail.com>
2026-05-06 17:09:46 -0300

    Quase Intervalos

--- player.py
+++ player.py
@@ -1,51 +1,29 @@
 ### TODO: PREENCHA SUAS INFORMAÇÕES AQUI ###
 # Nome #01 (quem entregou o código):    João Rodolfo Berger Andrade
 # RA #01 (quem entregou o código):      306480
-# Nome #02:                             [NOME COMPLETO #02]
-# RA #02:                               [RA #02]
+# Nome #02:                             Felipe de Lucca
+# RA #02:                               312860
 
-"""Implemente aqui o seu código para adivinhar a regra.
-
-Seu principal objetivo é implementar a função `player`, que deve retornar sua ação na rodada (chute de número ou chute de regra) e seu chute.
-1. Se for um chute de número, ele deve ser um inteiro entre 1 e 100.000.
-2. Se for um chute de regra, ele deve ser uma lista do tipo [TIPO, P1, P2], onde:
-    - TIPO é uma string que pode ser "mod", "pot" ou "int", indicando o tipo da regra;
-    - P1 e P2 são os parâmetros (números inteiros) da regra, que dependem do tipo.
-        - Se TIPO for "mod", P1 é o valor de k e P2 é o valor de r.
-        - Se TIPO for "pot", P1 é o valor de p. P2 é ignorado e pode ser qualquer valor.
-        - Se TIPO for "int", P1 é o valor de a e P2 é o valor de b.
-
-Exemplos de retornos válidos da função `player`:
-- ["NUMBER", 42]             Chutando o número 42
-- ["NUMBER", 100000]         Chutando o número 100.000
-- ["RULE", ["mod", 3, 1]]    Chutando a regra "n mod 3 dá resto 1"
-- ["RULE", ["pot", 2, 999]]  Chutando a regra "n é potência perfeita de ordem 2"
-- ["RULE", ["int", 10, 20]]  Chutando a regra "n pertence ao intervalo [10, 20]"
-
-Caso sua função não tenha um retorno adequado, a automatização não irá ocorrer tanto em game.py quanto em tournament.py.
-
----
-
-A função `player` recebe duas listas como argumentos:
-- number_guesses: lista de respostas aos chutes de número anteriores, onde cada elemento é uma lista do tipo [chute, direção, acerto], sendo:
-    - chute:            o número inteiro chutado
-    - direção:          a direção que indica se um número mais próximo que satisfaz a regra é maior ou menor do que o chute,
-        sendo "igual" se o chute satisfizer a regra e menor se o chute estiver exatamente entre dois números que satisfazem a regra
-    - acerto:           booleano indicando se o chute satisfaz a regra ou não
-
-- rule_guesses: lista de respostas aos chutes de regras anteriores, onde cada elemento é uma lista do tipo [TIPO, P1, P2], 
-    que significam a mesma coisa que os elementos do chute de regra descritos mais acima
-
-Você pode implementar outras funções para auxiliar a função `player` e salvar informações entre os chutes usando variáveis globais (fora de qualquer função).
-
-Para mais informações, verifique o README.md ou consulte um monitor.
-"""
 
 CHUTE_DE_NUMERO = "NUMBER"
 CHUTE_DE_REGRA = "RULE"
 parar_busca = False
 testar_sucessor = True
-testar_antecessor = True
+testar_antecessor = False
+testar_intervalo_unico = True
+antecessor_testado = False
+antecessor_acabou_de_ser_testado = False
+sucessor_acabou_de_ser_testado = False
+first_high_search = True
+first_low_search = True
+high = -1
+low = -1
+last_right_guess = -1
+last_not_right_guess = -1
+muito_provavelmente_intervalo = False
+
+first = True
+second = True
 
 def player(number_guesses, rule_guesses):
     #O primeiro chute do programa será 50_000
@@ -55,15 +33,23 @@ def player(number_guesses, rule_guesses):
     #O programa irá chegar se o chute não está contido na regra...
     global parar_busca
 
+    global gap
+
     if not number_guesses[-1][2] and not parar_busca:
         #...e se não estiver, irá definir um intervalo para fazer uma busca binária
-        global gap
 
         if len(number_guesses) == 1:
             gap = 25_000
         else:
             #Divide o intervalo em dois
             gap//=2
+
+        #Se a gap for zero, o número ou será o sucessor ou o antecessor do último chute
+        if gap == 0:
+            if number_guesses[-1][1] == 'menor':
+                return [CHUTE_DE_NUMERO, number_guesses[-1][0]-1]
+            else:
+                return [CHUTE_DE_NUMERO, number_guesses[-1][0]+1]
 
         if number_guesses[-1][1] == 'menor':
             return [CHUTE_DE_NUMERO, number_guesses[-1][0]-gap]
@@ -72,21 +58,140 @@ def player(number_guesses, rule_guesses):
     
     #Aqui, a busca binária foi feita e encontramos o primeiro elemento que satisfaz a regra
     else:
-        global testar_sucessor
+        if not parar_busca:
+            global number_from_search
+            number_from_search = number_guesses[-1][0]
         parar_busca = True
     
     #Vamos checar se a regra é de intervalos testando o sucessor do nosso primeiro elemento
+    global muito_provavelmente_intervalo
+
+    global testar_sucessor
+    global sucessor_acabou_de_ser_testado
+    global testar_antecessor
+
     if testar_sucessor:
         testar_sucessor = False
-        global testar_antecessor
-        return [CHUTE_DE_NUMERO, number_guesses[-1][0]+1]
+        sucessor_acabou_de_ser_testado = True
+        return [CHUTE_DE_NUMERO, number_from_search+1]
+    
+    if sucessor_acabou_de_ser_testado and number_guesses[-1][2]:
+        sucessor_acabou_de_ser_testado = False
+        muito_provavelmente_intervalo = True
+    elif sucessor_acabou_de_ser_testado and not number_guesses[-1][2]:
+        sucessor_acabou_de_ser_testado = False
+        testar_antecessor = True
     
     #Para não correr o risco de assumir que a regra não é intervalo e o número for o último do intervalo, testaremos o antecessor
+    global antecessor_testado
+    global antecessor_acabou_de_ser_testado
+
     if not number_guesses[-1][2] and testar_antecessor:
+        antecessor_testado = True
+        antecessor_acabou_de_ser_testado = True
         testar_antecessor = False
-        return [CHUTE_DE_NUMERO, number_guesses[-1][0]-2]
+        return [CHUTE_DE_NUMERO, number_from_search-1]
+   
+    if antecessor_testado and number_guesses[-1][2] and antecessor_acabou_de_ser_testado:
+        antecessor_acabou_de_ser_testado = False
+        muito_provavelmente_intervalo = True
     
+    elif antecessor_acabou_de_ser_testado and not number_guesses[-1][2]:
+        antecessor_acabou_de_ser_testado = False
+        muito_provavelmente_intervalo = False
+    
+    #Se o sucessor ou antecessor são parte também da regra, muito provavelmente é um intervalo (a não ser resto 0 mod 1)
+    if muito_provavelmente_intervalo:
+        global high
+        global low
+        global first_high_search
+        global first_low_search
+        global last_not_right_guess
+        global last_right_guess
+
+        #Note que se o antecessor foi testado, o sucessor não era parte e, assim, o último elemento do intervalo é o próprio número da busca binária
+        if antecessor_testado:
+            high = number_from_search
+            print(high)
+        
+        if high == -1: #Foi definido por padrão como -1, se não for, achamos o maior número do intervalo
+            if first_high_search:
+                #O primeiro chute da busca binária será o número + 50
+                first_high_search = False
+                gap = 50
+                print(number_from_search+gap)
+                return [CHUTE_DE_NUMERO, number_from_search+gap]
+            else:
+                #Busca dos próximos chutes
+
+                if last_not_right_guess == -1 or last_right_guess == -1:
+                    gap //= 2
+                
+                if gap != 0:
+                    if number_guesses[-1][2]:
+                        last_right_guess = number_guesses[-1][0]
+                        print(number_guesses[-1][0]+gap)
+                        if last_not_right_guess != -1 and last_right_guess != -1:
+                            gap = (last_not_right_guess - last_right_guess)//2
+                        return [CHUTE_DE_NUMERO, number_guesses[-1][0]+gap]
+                    else:
+                        last_not_right_guess = number_guesses[-1][0]
+                        print(number_guesses[-1][0]-gap)
+                        if last_not_right_guess != -1 and last_right_guess != -1:
+                            gap = (last_not_right_guess - last_right_guess)//2
+                        return [CHUTE_DE_NUMERO, number_guesses[-1][0]-gap]
+                
+                else:
+                    #Se a gap for igual a 0, o número ou vai ser o próprio número, ou o anterior
+                    if number_guesses[-1][2]:
+                        high = number_guesses[-1][0]
+                    else:
+                        high = number_guesses[-1][0]-1
+                    print(high)
+                    print(number_guesses[-1][0])
+        
+        if low == -1:
+            if first_low_search:
+                first_low_search = False
+                gap = 100 - (high - number_from_search)
+                return [CHUTE_DE_NUMERO, number_from_search-gap]
+            else:
+                if last_not_right_guess == -1 or last_right_guess == -1:
+                    gap //= 2
+                
+                if gap != 0:
+                    if number_guesses[-1][2]:
+                        last_right_guess = number_guesses[-1][0]
+                        if last_not_right_guess != -1 and last_right_guess != -1:
+                            gap = (last_right_guess - last_not_right_guess)//2
+                        return [CHUTE_DE_NUMERO, number_guesses[-1][0]-gap]
+                    else:
+                        last_not_right_guess = number_guesses[-1][0]
+                        if last_not_right_guess != -1 and last_right_guess != -1:
+                            gap = (last_right_guess - last_not_right_guess)//2
+                        return [CHUTE_DE_NUMERO, number_guesses[-1][0]+gap]
+                else:
+                    #Se a gap for igual a 0, o número ou vai ser o próprio número, ou o próximo
+                    if number_guesses[-1][2]:
+                        low = number_guesses[-1][0]
+                    else:
+                        low = number_guesses[-1][0]+1
+                    print(low)
+        
+        global first, second
+        if first:
+            first = False
+            return [CHUTE_DE_REGRA, ["int", low, high]]
+        elif second:
+            second = False
+            return [CHUTE_DE_REGRA, ["int", low, high-2]]
+
+        #return [CHUTE_DE_REGRA, ["int", low, high]]
+
     else:
-        testar_antecessor = False
-    
-    
+        #Testar se não é o intervalo [n,n]
+        global testar_intervalo_unico
+        if testar_intervalo_unico:
+            testar_intervalo_unico = False
+            return [CHUTE_DE_REGRA, ["int", number_from_search, number_from_search]]
+        
